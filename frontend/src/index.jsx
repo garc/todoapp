cf65c9bbf0de0614530a9464bd448389760d1a08
gabriel <gabriel.archangelo@gmail.com>
2017-10-31 23:13:38 -0200

    'teste'

---
+++ frontend/src/index.jsx
@@ -0,0 +1,5 @@
+import Reacet from 'react'
+import ReactDOM from 'react-dom'
+import App from './main/app'
+
+ReactDOM.render( <App />, document.getElementById('app'))
---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,20 @@
+/*import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+export default props => (
+    <div className='container'>
+        <h1>Teste</h1>
+    </div>
+)*/
+
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+    
+export default props => (
+    <div className='container'>
+        <h1>Teste AAAA</h1>
+    </div>
+)
